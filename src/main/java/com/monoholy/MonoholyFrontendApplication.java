4b321889bd95379a7888281943a5f272406b98a1
holynekk <e238109@metu.edu.tr>
2023-11-28 02:08:13 +0300

    initial files

---
+++ src/main/java/com/monoholy/MonoholyFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.monoholy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoholyFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonoholyFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monoholy/MonoholyFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monoholy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoholyFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
